c0db5db9bddd32f4c441b100ac423a00e94d19f9
Tanis Pérez Nartallo <tanisperez@gmail.com>
2025-02-13 23:19:36 +0100

    Add kitty config

--- setup.sh
+++ setup.sh
@@ -3,3 +3,7 @@
 # zed
 rm -f ~/.config/zed/settings.json
 ln -s ~/dotfiles/zed/settings.json ~/.config/zed/settings.json
+
+# kitty
+rm -f ~/dotfiles/.config/kitty/custom.conf
+ln -s ~/dotfiles/kitty/custom.conf ~/dotfiles/.config/kitty/custom.conf

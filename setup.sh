b8d4ac3e432241d6f2e6d20d6d77904dc5fda256
Tanis Pérez Nartallo <tanisperez@gmail.com>
2025-02-18 18:19:13 +0100

    Add echoes messages to setup.sh

--- setup.sh
+++ setup.sh
@@ -1,23 +1,30 @@
 #!/usr/bin/env bash
 
 # ssh
+echo -e "\e[1;32mSetting up ssh...\e[0m"
 rm -f ~/.ssh/config
 ln -s ~/dotfiles/ssh/config ~/.ssh/config
 
 # zed
+echo -e "\e[1;32mSetting up Zed...\e[0m"
 rm -f ~/.config/zed/settings.json
 ln -s ~/dotfiles/zed/settings.json ~/.config/zed/settings.json
 
 # kitty
+echo -e "\e[1;32mSetting up Kitty...\e[0m"
 rm -f ~/dotfiles/.config/kitty/custom.conf
 ln -s ~/dotfiles/kitty/custom.conf ~/dotfiles/.config/kitty/custom.conf
 
 # waybar + ML4W
+echo -e "\e[1;32mSetting up Waybar theme...\e[0m"
 rm -f ~/dotfiles/.config/waybar/themes/tanis
 ln -s ~/dotfiles/waybar/themes/tanis ~/dotfiles/.config/waybar/themes/tanis
 echo "/tanis;/tanis/white" > ~/dotfiles/.config/ml4w/settings/waybar-theme.sh
 
-# hypr + ML4W
+# Hyprland + ML4W
+echo -e "\e[1;32mSetting up Hyprland decorations...\e[0m"
 rm -f ~/dotfiles/.config/hypr/conf/decorations/tanis-decorations.conf
 ln -s ~/dotfiles/hypr/decorations.conf ~/dotfiles/.config/hypr/conf/decorations/tanis-decorations.conf
 echo "source = ~/.config/hypr/conf/decorations/tanis-decorations.conf" > ~/dotfiles/.config/hypr/conf/decoration.conf
+
+echo -e "\e[1;32mSetting up Hyprland keybindings...\e[0m"

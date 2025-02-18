21c6cb7764bb650b6a94ace367fc9099fb334209
Tanis Pérez Nartallo <tanisperez@gmail.com>
2025-02-18 18:15:11 +0100

    Add hypr decorations config

--- setup.sh
+++ setup.sh
@@ -12,7 +12,12 @@ ln -s ~/dotfiles/zed/settings.json ~/.config/zed/settings.json
 rm -f ~/dotfiles/.config/kitty/custom.conf
 ln -s ~/dotfiles/kitty/custom.conf ~/dotfiles/.config/kitty/custom.conf
 
-# waybar
+# waybar + ML4W
 rm -f ~/dotfiles/.config/waybar/themes/tanis
 ln -s ~/dotfiles/waybar/themes/tanis ~/dotfiles/.config/waybar/themes/tanis
 echo "/tanis;/tanis/white" > ~/dotfiles/.config/ml4w/settings/waybar-theme.sh
+
+# hypr + ML4W
+rm -f ~/dotfiles/.config/hypr/conf/decorations/tanis-decorations.conf
+ln -s ~/dotfiles/hypr/decorations.conf ~/dotfiles/.config/hypr/conf/decorations/tanis-decorations.conf
+echo "source = ~/.config/hypr/conf/decorations/tanis-decorations.conf" > ~/dotfiles/.config/hypr/conf/decoration.conf

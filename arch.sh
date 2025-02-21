c2319f8332875afc80b8e6e34768fb3d65a1deb2
Tanis Pérez Nartallo <tanisperez@gmail.com>
2025-02-21 22:51:52 +0100

    Add more dependencies to arch.sh

--- arch.sh
+++ arch.sh
@@ -3,6 +3,12 @@
 sudo pacman -S
     gnome-keyring \ # GNOME credentials
     seahorse \ # GNOME Keyring GUI
+    gvfs-smb \ # Nautilus SMB support
+    man \
+    less \
+    loupe \ # Light image viewer
+    blueberry # Bluetooth Manager
+
 
 systemctl --user enable --now gnome-keyring-daemon gcr-ssh-agent ssh-agent
 ssh-add ~/.ssh/github

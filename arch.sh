db6d0cd53576c8885c28f2267759a54cdf533ea7
Tanis Pérez Nartallo <tanisperez@gmail.com>
2025-02-21 22:49:18 +0100

    Update arch.sh script

--- arch.sh
+++ arch.sh
@@ -4,5 +4,5 @@ sudo pacman -S
     gnome-keyring \ # GNOME credentials
     seahorse \ # GNOME Keyring GUI
 
-systemctl --user enable ssh-agent
+systemctl --user enable --now gnome-keyring-daemon gcr-ssh-agent ssh-agent
 ssh-add ~/.ssh/github

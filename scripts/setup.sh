087098279e2dc4145225af176c6f939f66d29cbe
Tanis Pérez Nartallo <tanisperez@gmail.com>
2025-04-01 23:22:27 +0200

    Refactor scripts/setup.sh

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,39 +1,24 @@
 #!/usr/bin/env bash
 
-is_not_symlink() {
-    if [ -L "$1" ]; then
-        return 1  # not a symlink
-    else
-        return 0  # it's a symlink
-    fi
-}
-
-create_symlink() {
-    local config=$1
-    local source=$2
-    local target=$3
-
-    if is_not_symlink "$source"; then
-        if ln -sf "$target" "$source"; then
-            echo -e "\e[32m[✓] $config configured successfully\e[0m"
-        else
-            echo -e "\e[31m[✗] Error creating symlink for $config\e[0m"
-            return 1
-        fi
-    else
-        echo -e "\e[33m[!] $config symlink is already configured\e[0m"
-    fi
-}
-
 create_symlink_with_backup() {
     local config=$1
     local source=$2
     local target=$3
+    local backup_dir="$HOME/.config/backup"
 
-    if is_not_symlink "$source"; then
-        if mv "$source" "$source.bak" 2>/dev/null; then
+    # If exists and not a symlink
+    if [ -e "$source" ] && [ ! -L "$source" ]; then
+        if ! mkdir -p "$backup_dir" 2>/dev/null; then
+            echo -e "\e[31m[✗] Error creating backup directory\e[0m"
+            return 1
+        fi
+
+        local backup_name="$(basename "$source")_bak"
+        local backup_path="$backup_dir/$backup_name"
+
+        if mv "$source" "$backup_path" 2>/dev/null; then
             if ln -sf "$target" "$source"; then
-                echo -e "\e[32m[✓] $config configured successfully\e[0m"
+                echo -e "\e[32m[✓] $config configured successfully (backup in $backup_path)\e[0m"
             else
                 echo -e "\e[31m[✗] Error creating symlink for $config\e[0m"
                 return 1
@@ -43,7 +28,17 @@ create_symlink_with_backup() {
             return 1
         fi
     else
-        echo -e "\e[33m[!] $config symlink is already configured\e[0m"
+        # Remove source if it is a symlink
+        if [ -L "$source" ]; then
+            rm "$source"
+        fi
+
+        if ln -sf "$target" "$source"; then
+            echo -e "\e[32m[✓] $config configured successfully\e[0m"
+        else
+            echo -e "\e[31m[✗] Error creating symlink for $config\e[0m"
+            return 1
+        fi
     fi
 }
 
@@ -65,19 +60,19 @@ create_symlink_with_backup "ssh" ~/.ssh/config ~/dotfiles/ssh/config
 # zed
 create_symlink_with_backup "zed" ~/.config/zed/settings.json ~/dotfiles/zed/settings.json
 # kitty
-create_symlink_with_backup "kitty" ~/.config/kitty/kitty.conf ~/dotfiles/kitty/kitty.conf
+create_symlink_with_backup "kitty" ~/.config/kitty ~/dotfiles/kitty
 # nano
-create_symlink_with_backup "nano" ~/.config/nano/nanorc ~/dotfiles/nano/nanorc
+create_symlink_with_backup "nano" ~/.config/nano ~/dotfiles/nano
 # oh-my-zsh
 create_symlink_with_backup "oh-my-zsh" ~/.zshrc ~/dotfiles/zsh/zshrc
 # gtk-3.0 settings
-create_symlink_with_backup "gtk-3.0 settings" ~/.config/gtk-3.0/settings.ini ~/dotfiles/gtk-3.0/settings.ini
+create_symlink_with_backup "gtk-3.0 settings" ~/.config/gtk-3.0 ~/dotfiles/gtk-3.0
 # gtk-4.0 settings
-create_symlink_with_backup "gtk-4.0 settings" ~/.config/gtk-4.0/settings.ini ~/dotfiles/gtk-4.0/settings.ini
+create_symlink_with_backup "gtk-4.0 settings" ~/.config/gtk-4.0 ~/dotfiles/gtk-4.0
 # hyprland
-create_symlink "hyprland" ~/.config/hypr ~/dotfiles/hypr
+create_symlink_with_backup "hyprland" ~/.config/hypr ~/dotfiles/hypr
 # wlogout
-create_symlink "wlogout" ~/.config/wlogout ~/dotfiles/wlogout 
+create_symlink_with_backup "wlogout" ~/.config/wlogout ~/dotfiles/wlogout 
 
 #########################
 # root stuff

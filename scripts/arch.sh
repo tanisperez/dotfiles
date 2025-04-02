35458a063d28aa8a94e8ff12eb3c5929c3133020
Tanis Pérez Nartallo <tanisperez@gmail.com>
2025-04-02 23:39:35 +0200

    Add zsh-autosuggestions zsh plugin

--- scripts/arch.sh
+++ scripts/arch.sh
@@ -62,6 +62,7 @@ yay -S "${yay_packages[@]}"
 # oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k"
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
 #systemctl --user enable --now gnome-keyring-daemon gcr-ssh-agent ssh-agent
 #ssh-add ~/.ssh/github

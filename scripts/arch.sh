f835a6f913d175d4d3af58d5fd112cca55820a70
Tanis Pérez Nartallo <tanisperez@gmail.com>
2025-04-02 22:58:13 +0200

    Update scripts/arch.sh

--- scripts/arch.sh
+++ scripts/arch.sh
@@ -1,47 +1,49 @@
-#!/usr/bin/env bah
+#!/usr/bin/env bash
 
 # Update the current system
 sudo pacman -Syu
 
-sudo pacman -S
-    gnome-keyring \ # GNOME credentials
-    seahorse \ # GNOME Keyring GUI
-    gvfs-smb \ # Nautilus SMB support
-    man \
-    tree \
-    less \
-    htop \
-    btop \
-    nvtop \
-    neofetch \
-    loupe \ # Light image viewer
-    evince \ # PDF viewer
-    fzf \ # Fuzzy finder
-    pinta \ # Image editor
-    zed \ # Zed text editor
-    nwg-displays \ # Hyprland Display manager
-    nwg-look \ # Hyprland Look manager
-    pacseek \ # Pacman GUI
-    cliphist \ # Clipboard history
-    blueberry \ # Bluetooth Manager
-    gnome-calculator \
-    vlc \
-    gnome-disk-utility \ # Disk manager
-    qt6-virtualkeyboard \ # Virtual Keyboard for SDDM
-    transmission-gtk \ # Torrent client
-    swaync \ # Notifications
-    rofi \
-    rofi-emoji \
-    ttf-jetbrains-mono-nerd \ # JetBrains Mono Nerd Font
-    ttf-montserrat \ # Montserrat Font
-    ttf-fira-sans \ # Fira Sans Font
-    ttf-font-awesome \ # Font Awesome
-    xdg-desktop-portal-hyprland \ # Screen casting for Hyprland
-    wlogout # Wayland logout menu
+packages=(
+    gnome-keyring     # GNOME credentials
+    seahorse          # GNOME Keyring GUI
+    gvfs-smb          # Nautilus SMB support
+    man
+    tree
+    less
+    htop
+    btop
+    nvtop
+    neofetch
+    loupe            # Light image viewer
+    evince           # PDF viewer
+    fzf              # Fuzzy finder
+    pinta            # Image editor
+    zed              # Zed text editor
+    nwg-displays     # Hyprland Display manager
+    nwg-look         # Hyprland Look manager
+    pacseek          # Pacman GUI
+    cliphist         # Clipboard history
+    blueberry        # Bluetooth Manager
+    gnome-calculator
+    vlc
+    gnome-disk-utility     # Disk manager
+    qt6-virtualkeyboard    # Virtual Keyboard for SDDM
+    transmission-gtk       # Torrent client
+    swaync               # Notifications
+    rofi
+    rofi-emoji
+    ttf-jetbrains-mono-nerd  # JetBrains Mono Nerd Font
+    ttf-montserrat          # Montserrat Font
+    ttf-fira-sans          # Fira Sans Font
+    ttf-font-awesome       # Font Awesome
+    xdg-desktop-portal-hyprland  # Screen casting for Hyprland
+    wlogout               # Wayland logout menu
+)
 
-yay -S
-    # smile \ # emoji picker
-    bibata-cursor-theme-bin # cursor theme
+
+sudo pacman -S "${packages[@]}"
+
+yay -S bibata-cursor-theme-bin # cursor theme
 
 systemctl --user enable --now gnome-keyring-daemon gcr-ssh-agent ssh-agent
 ssh-add ~/.ssh/github
